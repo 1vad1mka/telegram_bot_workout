39941c3b417f0eb32c6b5e6bf8d184fb0e75e824
Vadim Khibin <vadimhibin@gmail.com>
2025-01-22 14:38:16 +0300

    added logging

--- app/middleware.py
+++ app/middleware.py
@@ -1,8 +1,11 @@
 from aiogram import BaseMiddleware
 from aiogram.types import Message
+import logging
+
+# Настройка логирования
+logging.basicConfig(level=logging.INFO)
 
 class LoggingMiddleware(BaseMiddleware):
     async def __call__(self, handler, event: Message, data: dict):
-        print(f"Получено сообщение: {event.text}")
+        logging.info(f"Получено сообщение: {event.text}")
         return await handler(event, data)
-
